c3f900d389074ef6522132e367cd5ed1dc9fcbc6
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-17 18:57:10 +0100

    ex11

---
+++ week-01/day04/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char* args[]) {
+
+    // - Create an array variable named `t`
+    //    with the following content: `[1, 2, 3, 4, 5]`
+    // - Increment the third element
+    // - Print the third element
+    int t [5] {1, 2, 3, 4, 5};
+    int a = t [2];
+    std::cout << a << std::endl;
+    return 0;
+}

593811cdbb884ba738375e3e8f62b9d61f65478c
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-19 15:27:46 +0100

    colors

---
+++ week-01/day05/colors/cmake-build-debug/CMakeFiles/colors.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/colors.dir/main.cpp.obj"
+  "colors.pdb"
+  "colors.exe"
+  "colors.exe.manifest"
+  "libcolors.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/colors.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

d418ae71b11d6325b937b4dc301d08e8499c4c99
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-22 11:50:14 +0100

    owerwriteValue

--- week-02/day02/changeValue/main.cpp
+++ week-02/day02/changeValue/main.cpp
@@ -6,6 +6,10 @@ int main ()
     // (you have to create the pointer first)
 
     float temperature = 21.3;
+    float *p_temperature = &temperature;
+    std::cout << *p_temperature << std::endl;
+    *p_temperature = 10;
+    std::cout << *p_temperature << std::endl;
 
     return 0;
 }

7b7a1f65a658aaca56bf48db749f11866d9c93cc
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-23 17:27:23 +0100

    printmultiplelines

--- week-02/day03/writeMultipleLines/main.cpp
+++ week-02/day03/writeMultipleLines/main.cpp
@@ -14,22 +14,30 @@ void function(std::string file, std::string string, int numberOfLines);
 
 int main() {
 
-    std::string string = "nibba";
+    std::string const string = "nibba\n";
     int numberOfLines = 5;
     std::ofstream file("text.txt");
     std::string fileName = "text.txt";
     function(fileName, string, numberOfLines);
     file.close();
-    
+
+    std::ifstream fileStream("text.txt");
+    std::string line;
+
+    if (fileStream.is_open()) {
+        while (getline(fileStream, line)) {
+            std::cout << line << '\n';
+        }
+        fileStream.close();
     }
     return 0;
 }
 
 
-void function(std::string file, std::string string, int numberOfLines){
+void function(std::string file, std::string string, int numberOfLines) {
     std::ofstream newFile(file);
     std::string line;
-    for(int i = 1; i <= numberOfLines; i++){
+    for (int i = 1; i <= numberOfLines; i++) {
         newFile << string;
     }
 

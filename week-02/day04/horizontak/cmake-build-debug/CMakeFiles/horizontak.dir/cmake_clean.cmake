50c85c769e4393dfadc8efbbfab0ffc295409f47
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-24 15:10:12 +0100

    horizontal

---
+++ week-02/day04/horizontak/cmake-build-debug/CMakeFiles/horizontak.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/horizontak.dir/main.cpp.obj"
+  "horizontak.pdb"
+  "horizontak.exe"
+  "horizontak.exe.manifest"
+  "libhorizontak.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/horizontak.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day04/horizontak/cmake-build-debug/CMakeFiles/horizontak.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for horizontak.
+# This may be replaced when dependencies are built.

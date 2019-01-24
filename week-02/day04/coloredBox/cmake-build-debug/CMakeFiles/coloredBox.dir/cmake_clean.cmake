a3d15441c0203c08052104d0adaf15c525c192bf
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-24 14:17:44 +0100

    drawBoxW4colors

---
+++ week-02/day04/coloredBox/cmake-build-debug/CMakeFiles/coloredBox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coloredBox.dir/main.cpp.obj"
+  "coloredBox.pdb"
+  "coloredBox.exe"
+  "coloredBox.exe.manifest"
+  "libcoloredBox.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/coloredBox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

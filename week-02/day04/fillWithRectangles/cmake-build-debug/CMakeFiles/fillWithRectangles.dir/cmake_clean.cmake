595da18051b4bea1d18d822e48b91f99f4a7f87e
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-24 16:26:11 +0100

    fourRectangles

---
+++ week-02/day04/fillWithRectangles/cmake-build-debug/CMakeFiles/fillWithRectangles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fillWithRectangles.dir/main.cpp.obj"
+  "fillWithRectangles.pdb"
+  "fillWithRectangles.exe"
+  "fillWithRectangles.exe.manifest"
+  "libfillWithRectangles.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fillWithRectangles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

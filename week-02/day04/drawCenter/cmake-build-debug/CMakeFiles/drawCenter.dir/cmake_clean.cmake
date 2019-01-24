7847f994d85d612f87ce3ab602a6bd241f9ba3b0
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-24 14:46:50 +0100

    drawCenter

---
+++ week-02/day04/drawCenter/cmake-build-debug/CMakeFiles/drawCenter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/drawCenter.dir/main.cpp.obj"
+  "drawCenter.pdb"
+  "drawCenter.exe"
+  "drawCenter.exe.manifest"
+  "libdrawCenter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/drawCenter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-02/day04/drawCenter/cmake-build-debug/CMakeFiles/drawCenter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for drawCenter.
+# This may be replaced when dependencies are built.

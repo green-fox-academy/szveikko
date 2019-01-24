76be1d0ff1915d59acc7c870fd697b28429bcc96
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-24 13:52:43 +0100

    drawLine

---
+++ week-02/day04/middleLines/cmake-build-debug/CMakeFiles/middleLines.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/middleLines.dir/main.cpp.obj"
+  "middleLines.pdb"
+  "middleLines.exe"
+  "middleLines.exe.manifest"
+  "libmiddleLines.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/middleLines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

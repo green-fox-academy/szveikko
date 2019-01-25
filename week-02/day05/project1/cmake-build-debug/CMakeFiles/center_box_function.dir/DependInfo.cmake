d5607d2692f1da0a346526a6f9cc6555b401d486
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-01-25 12:15:44 +0100

    project1

---
+++ week-02/day05/project1/cmake-build-debug/CMakeFiles/center_box_function.dir/DependInfo.cmake
@@ -0,0 +1,40 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_framerate.c" "C:/Users/veikk/greenfox/greenfox/week-02/day05/project1/cmake-build-debug/CMakeFiles/center_box_function.dir/C_/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_framerate.c.obj"
+  "C:/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_gfxPrimitives.c" "C:/Users/veikk/greenfox/greenfox/week-02/day05/project1/cmake-build-debug/CMakeFiles/center_box_function.dir/C_/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_gfxPrimitives.c.obj"
+  "C:/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_imageFilter.c" "C:/Users/veikk/greenfox/greenfox/week-02/day05/project1/cmake-build-debug/CMakeFiles/center_box_function.dir/C_/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_imageFilter.c.obj"
+  "C:/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_rotozoom.c" "C:/Users/veikk/greenfox/greenfox/week-02/day05/project1/cmake-build-debug/CMakeFiles/center_box_function.dir/C_/DewTools/SDL/SDL2_plugins/gfx/src/SDL2_rotozoom.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/DewTools/SDL/SDL2-2.0.8/i686-w64-mingw32/include/SDL2"
+  "C:/DewTools/SDL/SDL2_image-2.0.3/i686-w64-mingw32/include/SDL2"
+  "C:/DewTools/SDL/SDL2_ttf-2.0.14/i686-w64-mingw32/include/SDL2"
+  "C:/DewTools/SDL/SDL2_plugins/gfx/inc"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/veikk/greenfox/greenfox/week-02/day05/project1/main.cpp" "C:/Users/veikk/greenfox/greenfox/week-02/day05/project1/cmake-build-debug/CMakeFiles/center_box_function.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "C:/DewTools/SDL/SDL2-2.0.8/i686-w64-mingw32/include/SDL2"
+  "C:/DewTools/SDL/SDL2_image-2.0.3/i686-w64-mingw32/include/SDL2"
+  "C:/DewTools/SDL/SDL2_ttf-2.0.14/i686-w64-mingw32/include/SDL2"
+  "C:/DewTools/SDL/SDL2_plugins/gfx/inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

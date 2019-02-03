6904b54f30aef6d03bd247af8d588dbaf556934a
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-02-03 16:52:58 +0100

    needsSomeRepairr

---
+++ week-03/weekend/blog/cmake-build-debug/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_ba1f3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_ba1f3.dir/feature_tests.cxx.obj"
+  "cmTC_ba1f3.pdb"
+  "cmTC_ba1f3.exe"
+  "cmTC_ba1f3.exe.manifest"
+  "libcmTC_ba1f3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmTC_ba1f3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

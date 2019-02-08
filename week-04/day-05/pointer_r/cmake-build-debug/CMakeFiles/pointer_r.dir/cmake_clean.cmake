2a27e2f7c154b2fcffa716076c9b28be9d581d67
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-02-08 13:22:50 +0100

    pointerer

---
+++ week-04/day-05/pointer_r/cmake-build-debug/CMakeFiles/pointer_r.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pointer_r.dir/main.cpp.obj"
+  "pointer_r.pdb"
+  "pointer_r.exe"
+  "pointer_r.exe.manifest"
+  "libpointer_r.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pointer_r.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week-04/day-05/pointer_r/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    //address- of operator
+    int example_number1 = 100;
+    //The value is
+    std::cout << example_number1 << std::endl;
+    //The memory address is
+    std::cout << &example_number1 << std::endl;
+    //Pointer
+    int *p_example_number1 = &example_number1;
+    std::cout << p_example_number1 << std::endl;
+    //Pointer to value
+    std::cout << *p_example_number1 << std::endl;
+    int age = 66;
+    int *p_age = &age;
+    //should print put the value of age
+    std::cout << *p_age << std::endl;
+    //exercise print_adresses
+    std::cout << "Give me 5 numbers" << std::endl;
+    int num1, num2, num3, num4, num5;
+    std::cin >> num1;
+    std::cin >> num2;
+    std::cin >> num3;
+    std::cin >> num4;
+    std::cin >> num5;
+    std::vector<int> vectorOfNumbers;
+    vectorOfNumbers.push_back(num1);
+    vectorOfNumbers.push_back(num2);
+    vectorOfNumbers.push_back(num3);
+    vectorOfNumbers.push_back(num4);
+    vectorOfNumbers.push_back(num5);
+    //print out the addresses of the vector elements.
+    for (int i = 0; i < vectorOfNumbers.size(); i++) {
+        std::cout << "The memory address of item " << i << " is: " << &vectorOfNumbers[i] << std::endl;
+
+    }
+    int bigNum = 0;
+    int* p_bigNum = &bigNum;
+    for (int i = 0; i < vectorOfNumbers.size(); i++) {
+        if (vectorOfNumbers[i] > bigNum){
+            bigNum = vectorOfNumbers[i];
+        }
+    }
+    std::cout << "biNum = " << bigNum << std::endl;
+    std::cout << "Vector size = " << vectorOfNumbers.size() << std::endl;
+    std::cout << "The biggest number of the vector: " << *p_bigNum << std::endl;
+}

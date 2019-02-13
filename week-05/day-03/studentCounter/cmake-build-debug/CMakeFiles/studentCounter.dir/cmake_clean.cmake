6e85e698c29cb6f3cb93e5d87fdb09d08cc734e6
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-02-13 13:17:35 +0100

    pedo

---
+++ week-05/day-03/studentCounter/cmake-build-debug/CMakeFiles/studentCounter.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/studentCounter.dir/main.cpp.obj"
+  "studentCounter.pdb"
+  "studentCounter.exe"
+  "studentCounter.exe.manifest"
+  "libstudentCounter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/studentCounter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

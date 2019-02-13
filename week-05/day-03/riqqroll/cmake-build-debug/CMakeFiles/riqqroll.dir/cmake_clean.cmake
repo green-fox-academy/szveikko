17ef00deb6f6c70b59a8601b6975e53cebf65418
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-02-13 11:49:58 +0100

    riqqroll

---
+++ week-05/day-03/riqqroll/cmake-build-debug/CMakeFiles/riqqroll.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/riqqroll.dir/main.cpp.obj"
+  "riqqroll.pdb"
+  "riqqroll.exe"
+  "riqqroll.exe.manifest"
+  "libriqqroll.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/riqqroll.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

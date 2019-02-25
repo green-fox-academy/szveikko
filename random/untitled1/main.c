e7184ea3b72ca3e90ee8a0f0b14a02db43b28f3a
Szabó Veikko Bálint <veikko.szabo@gmail.com>
2019-02-25 12:30:55 +0100

    ex03

--- random/untitled1/main.c
+++ random/untitled1/main.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 
-int main()
-{
+int main() {
     // Modify this program to greet you instead of the World!
     printf("Hello, Veikko!\n");
 
@@ -33,7 +32,22 @@ int main()
     printf("%d\n", 31);
     printf("%f\n", 1.80);
 
-    
-
+// Create a program that prints a few operations on two numbers: 22 and 13
+    printf("%d %d \n", 22, 13);
+    // Print the result of 13 added to 22
+    printf("%d\n", (13+22));
+    // Print the result of 13 substracted from 22
+    printf("%d\n", 22-13);
+    // Print the result of 22 multiplied by 13
+    printf("%d\n", 22*13);
+    // Print the result of 22 divided by 13 (as a decimal fraction)
+    printf("%f\n", (22./13.));
+    // Print the reminder of 22 divided by 13
+    printf("%d\n", 22%13);
+    // Store the results in variables and use them when you display the result
+    int result_01 = 13+22;
+    int result_02 = 22-13;
+    int result_03 = 22*13;
+    printf("%d",result_01);
     return 0;
 }
